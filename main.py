7f5a951f788749fc286571a3e7ceaa8699d6a0be
OscarEpsilon <110500526+OscarEpsilon@users.noreply.github.com>
2024-04-23 15:52:47 -0400

    soup finds all strong tags

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import pandas
+import pandas as pd
 import requests as rq
 from bs4 import BeautifulSoup as bs
 
 url = "https://www.newschoolva.org/academics/course-catalog/"
 reqHeaders = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"}
 
-page = rq.get(url, headers=reqHeaders)
+pg = rq.get(url, headers=reqHeaders)
 
-print(page.text)
+pgHTML = pg.text
+soup = bs(pgHTML, "html5lib")
+
+headers = soup.find_all("strong")
+
+print(str(headers))
