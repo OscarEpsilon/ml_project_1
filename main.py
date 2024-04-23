87f9ad5d5605e03ce450af1b33b730c0584ec05e
wschlesingernsnva <131034953+wschlesingernsnva@users.noreply.github.com>
2024-04-23 15:48:35 -0400

    Fix get request

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas
 import requests as rq
+from bs4 import BeautifulSoup as bs
 
 url = "https://www.newschoolva.org/academics/course-catalog/"
+reqHeaders = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"}
 
-page = rq.get(url)
+page = rq.get(url, headers=reqHeaders)
 
 print(page.text)
