0d11fdc890665b7350b2a143e8e9e2137948c05c
wschlesingernsnva <131034953+wschlesingernsnva@users.noreply.github.com>
2024-04-24 17:45:58 -0400

    Fix poetry course and awkward spacing on collage studio

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import pandas as pd
 import requests as rq
 import bs4
 
-url = "https://www.newschoolva.org/academics/course-catalog/"
+catalogURL = "https://www.newschoolva.org/academics/course-catalog/"
 reqHeaders = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"}
 
-pg = rq.get(url, headers=reqHeaders)
-
-pgHTML = pg.text
-soup = bs4.BeautifulSoup(pgHTML, "html5lib")
+catalogRes = rq.get(catalogURL, headers=reqHeaders)
+catalogHTML = catalogRes.text
+catalogSoup = bs4.BeautifulSoup(catalogHTML, "html5lib")
 
 def getCourseInfo(titleTag: bs4.Tag) -> dict:
     # Some courses have their line break nested inside their strong tag instead of outside
@@ -25,9 +24,13 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
     if courseTitle == "Collage Studio":
         return {"title": courseTitle,
                 "credit": "Arts 1",
-                "desc": "Students will work with mixed media techniques to create various collage artworks. \
-                    Students can expect to work in 2D, 3D, and digital art mediums to develop their collages. \
-                    EQ: How can pre-existing images and materials be repurposed to create a new meaning?"}
+                "desc": "Students will work with mixed media techniques to create various collage artworks. Students can expect to work in 2D, 3D, and digital art mediums to develop their collages. EQ: How can pre-existing images and materials be repurposed to create a new meaning?"}
+    
+    # Also this one
+    if courseTitle == "Poetry, English 1":
+        return {"title": "Poetry",
+                "credit": "English 1",
+                "desc": "Poetry is an art that can be written, spoken, or performed–it is a total experience of language. To appreciate this art, we must learn the tools and techniques employed by the artists in its creation, and then study the end effect. You’ll be expected, early on, merely to respond to the poems in a human way. As we begin to share some ideas of what a poem is, what it does, and how, and why–we’ll explore the poems on a more sophisticated level. We will read a lot of poems, both traditional and those that break from tradition, and we will also craft and share our own poetry in an attempt to answer the question: How does the poet use all the tools at their disposal to create an experience in the reader?"}
 
     # Some courses have an unbolded letter at the start of their title
     prevSibling = titleTag.previous_sibling
@@ -49,8 +52,6 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
 
     courseCredit = ""
     courseDesc = ""
-
-    descSearchStartIndex = 1
     
     # If a course credit is given, it must be the next sibling with index 0, and it must have the str type
     # Otherwise, we know it is a math credit
@@ -67,7 +68,6 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
     
     else:
         courseCredit = "Math"
-        descSearchStartIndex = 0
     
     courseDescTags = []
 
@@ -82,7 +82,7 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
     
     return {"title": courseTitle, "credit": courseCredit, "desc": courseDesc}
 
-titleTags = soup.find_all("strong")
+titleTags = catalogSoup.find_all("strong")
 
 # One strong tag is an empty whitespace in the middle of the course catalog and should not be there
 for tagIndex, thisTag in enumerate(titleTags):
