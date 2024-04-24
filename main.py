ce391dbb3baab6e26772c2463532edce5ea84b70
wschlesingernsnva <131034953+wschlesingernsnva@users.noreply.github.com>
2024-04-24 17:56:08 -0400

    Fix dataframe headers, set to constants

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ catalogRes = rq.get(catalogURL, headers=reqHeaders)
 catalogHTML = catalogRes.text
 catalogSoup = bs4.BeautifulSoup(catalogHTML, "html5lib")
 
+COURSES_TITLE_KEY = "title"
+COURSES_CREDIT_KEY = "credit"
+COURSES_DESC_KEY = "description"
+
 def getCourseInfo(titleTag: bs4.Tag) -> dict:
     # Some courses have their line break nested inside their strong tag instead of outside
     brInsideStrong = False
@@ -22,15 +26,15 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
     # random empty strong tag placed in the middle of it--so
     # we just don't even bother to make exceptions for all of them here
     if courseTitle == "Collage Studio":
-        return {"title": courseTitle,
-                "credit": "Arts 1",
-                "desc": "Students will work with mixed media techniques to create various collage artworks. Students can expect to work in 2D, 3D, and digital art mediums to develop their collages. EQ: How can pre-existing images and materials be repurposed to create a new meaning?"}
+        return {COURSES_TITLE_KEY: courseTitle,
+                COURSES_CREDIT_KEY: "Arts 1",
+                COURSES_DESC_KEY: "Students will work with mixed media techniques to create various collage artworks. Students can expect to work in 2D, 3D, and digital art mediums to develop their collages. EQ: How can pre-existing images and materials be repurposed to create a new meaning?"}
     
     # Also this one
     if courseTitle == "Poetry, English 1":
-        return {"title": "Poetry",
-                "credit": "English 1",
-                "description": "Poetry is an art that can be written, spoken, or performed–it is a total experience of language. To appreciate this art, we must learn the tools and techniques employed by the artists in its creation, and then study the end effect. You’ll be expected, early on, merely to respond to the poems in a human way. As we begin to share some ideas of what a poem is, what it does, and how, and why–we’ll explore the poems on a more sophisticated level. We will read a lot of poems, both traditional and those that break from tradition, and we will also craft and share our own poetry in an attempt to answer the question: How does the poet use all the tools at their disposal to create an experience in the reader?"}
+        return {COURSES_TITLE_KEY: "Poetry",
+                COURSES_CREDIT_KEY: "English 1",
+                COURSES_DESC_KEY: "Poetry is an art that can be written, spoken, or performed–it is a total experience of language. To appreciate this art, we must learn the tools and techniques employed by the artists in its creation, and then study the end effect. You’ll be expected, early on, merely to respond to the poems in a human way. As we begin to share some ideas of what a poem is, what it does, and how, and why–we’ll explore the poems on a more sophisticated level. We will read a lot of poems, both traditional and those that break from tradition, and we will also craft and share our own poetry in an attempt to answer the question: How does the poet use all the tools at their disposal to create an experience in the reader?"}
 
     # Some courses have an unbolded letter at the start of their title
     prevSibling = titleTag.previous_sibling
@@ -80,7 +84,7 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
         if thisCourseDescTag.name != "br":
             courseDesc += thisCourseDescTag.string
     
-    return {"title": courseTitle, "credit": courseCredit, "desc": courseDesc}
+    return {COURSES_TITLE_KEY: courseTitle, COURSES_CREDIT_KEY: courseCredit, COURSES_DESC_KEY: courseDesc}
 
 titleTags = catalogSoup.find_all("strong")
 
