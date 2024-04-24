b672de8176ebc08dd86f73688f0fe53ac1380d86
wschlesingernsnva <131034953+wschlesingernsnva@users.noreply.github.com>
2024-04-24 13:52:10 -0400

    Work

--- main.py
+++ main.py
@@ -13,18 +13,35 @@ soup = bs4.BeautifulSoup(pgHTML, "html5lib")
 def getCourseInfo(titleTag: bs4.Tag) -> dict:
     # Some courses have their line break nested inside their strong tag instead of outside
     if len(titleTag.contents) == 2:
-        courseTitle = next(titleTag.children)
+        courseTitle = str(next(titleTag.children))
     else:
         courseTitle = titleTag.string
+    
+    # This is the only one with the unique exception of having a
+    # random empty strong tag placed in the middle of it--so
+    # we just don't even bother to make exceptions for all of them here
+    if courseTitle == "Collage Studio":
+        return {"title": courseTitle,
+                "credit": "Arts 1",
+                "desc": "Students will work with mixed media techniques to create various collage artworks. \
+                    Students can expect to work in 2D, 3D, and digital art mediums to develop their collages. \
+                    EQ: How can pre-existing images and materials be repurposed to create a new meaning?"}
 
     # Some courses have an unbolded letter at the start of their title
     prevSibling = titleTag.previous_sibling
     if prevSibling != None:
         courseTitle = prevSibling.string + courseTitle
     
+    numTitleEndBoldLetters = 0
+    
     # Some courses have a bolded comma at the end of the title
+    # Some also have an extra space as well
     if courseTitle[-1:] == ",":
         courseTitle = courseTitle[:-1]
+        numTitleEndBoldLetters = 1
+    elif courseTitle[-2:] == ", ":
+        courseTitle = courseTitle[:-2]
+        numTitleEndBoldLetters = 2
     
     courseInfo = list(titleTag.next_siblings)
 
@@ -36,19 +53,34 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
     # If a course credit is given, it must be the next sibling with index 0, and it must have the str type
     # Otherwise, we know it is a math credit
     if type(courseInfo[0]) == bs4.NavigableString:
-        courseCredit = courseInfo[0][2:]
+        courseCredit = str(courseInfo[0][(2 - numTitleEndBoldLetters):])
+        
+        # Some course credits end with a space
+        if courseCredit[-1:] == " ":
+            courseCredit = courseCredit[:-1]
+        
+        # Some end with " ("
+        if courseCredit[-2:] == " (":
+            courseCredit = courseCredit[:-2]
+    
     else:
         courseCredit = "Math"
         descSearchStartIndex = 0
     
     for thisInfo in courseInfo[descSearchStartIndex:]:
-        if type(thisInfo) == bs4.NavigableString:
-            courseDesc = thisInfo
+        if type(thisInfo) == bs4.NavigableString and str(thisInfo) != ")":
+            courseDesc = str(thisInfo)
             break
     
     return {"title": courseTitle, "credit": courseCredit, "desc": courseDesc}
 
 titleTags = soup.find_all("strong")
+
+# One strong tag is an empty whitespace in the middle of the course catalog and should not be there
+for tagIndex, thisTag in enumerate(titleTags):
+    if thisTag.string == " ":
+        del(titleTags[tagIndex])
+
 courseDicts = list(map(getCourseInfo, titleTags))
 courseDF = pd.DataFrame(courseDicts)
 courseDF.to_csv("courses.csv")
