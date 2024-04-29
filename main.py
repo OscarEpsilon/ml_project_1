f53640f66cc053655d2dc92e7209000236b4b586
OscarEpsilon <110500526+OscarEpsilon@users.noreply.github.com>
2024-04-29 14:29:03 -0400

    added types and fixed some bugs

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+from typing import Final
+
 import pandas as pd
 import requests as rq
 import bs4
 
-CATALOG_URL = "https://www.newschoolva.org/academics/course-catalog/"
-CATALOG_REQ_HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"}
+CATALOG_URL: Final[str] = "https://www.newschoolva.org/academics/course-catalog/"
+CATALOG_REQ_HEADERS: Final[dict[str, str]] = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"}
 
-COURSES_TITLE_KEY = "title"
-COURSES_CREDIT_KEY = "credit"
-COURSES_DESC_KEY = "description"
+COURSES_TITLE_KEY: Final[str] = "title"
+COURSES_CREDIT_KEY: Final[str] = "credit"
+COURSES_DESC_KEY: Final[str] = "description"
 
-catalogRes = rq.get(CATALOG_URL, headers=CATALOG_REQ_HEADERS)
-catalogHTML = catalogRes.text
-catalogSoup = bs4.BeautifulSoup(catalogHTML, "html5lib")
+catalogRes: Final[rq.Response] = rq.get(CATALOG_URL, headers=CATALOG_REQ_HEADERS)
+catalogHTML: Final[str] = catalogRes.text
+catalogSoup: Final[bs4.BeautifulSoup] = bs4.BeautifulSoup(catalogHTML, "html5lib")
 
-def getCourseInfo(titleTag: bs4.Tag) -> dict:
-    # Some courses have their line break nested inside their strong tag instead of outside
-    brInsideStrong = False
-    if len(titleTag.contents) == 2:
-        courseTitle = next(titleTag.children).string
+def getCourseInfo(titleTag: bs4.Tag) -> dict[str, str]:
+    courseTitle: str = ""
+
+    # Some courses have their line break nested inside their strong tag instead of outside, breaking titleTag.string
+    courseTitleAtt: str | None = titleTag.string
+    brInsideStrong: bool = False
+    if courseTitleAtt == None:
+        courseTitle = next(titleTag.children).string # type: ignore
         brInsideStrong = True
     else:
-        courseTitle = titleTag.string
-    
+        courseTitle = courseTitleAtt
+
     # There are a few weird individual exceptions
     # This is the only one with the unique exception of having a
     # random empty strong tag placed in the middle of it--so
@@ -30,13 +35,13 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
         return {COURSES_TITLE_KEY: courseTitle,
                 COURSES_CREDIT_KEY: "Arts 1",
                 COURSES_DESC_KEY: "Students will work with mixed media techniques to create various collage artworks. Students can expect to work in 2D, 3D, and digital art mediums to develop their collages. EQ: How can pre-existing images and materials be repurposed to create a new meaning?"}
-    
+
     # Also this one
     if courseTitle == "Poetry, English 1":
         return {COURSES_TITLE_KEY: "Poetry",
                 COURSES_CREDIT_KEY: "English 1",
                 COURSES_DESC_KEY: "Poetry is an art that can be written, spoken, or performed–it is a total experience of language. To appreciate this art, we must learn the tools and techniques employed by the artists in its creation, and then study the end effect. You’ll be expected, early on, merely to respond to the poems in a human way. As we begin to share some ideas of what a poem is, what it does, and how, and why–we’ll explore the poems on a more sophisticated level. We will read a lot of poems, both traditional and those that break from tradition, and we will also craft and share our own poetry in an attempt to answer the question: How does the poet use all the tools at their disposal to create an experience in the reader?"}
-    
+
     # And this one
     if courseTitle == "Mid-Century Crossroads":
         return {COURSES_TITLE_KEY: "Mid-Century Crossroads: The 1950s and 60s",
@@ -44,12 +49,12 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
                 COURSES_DESC_KEY: "The mid twentieth century witnessed intensely accelerated change in American society. Following World War II, political, economic, and social patterns led to widespread divisions within America. A time which appeared prosperous and idyllic on the surface came to an abrupt halt as Black Americans and women demanded civil rights, America’s youth became disillusioned, and conflicts arose between the generations. This course analyzes the causes and effects of this time period and its permanent impact on American culture."}
 
     # Some courses have an unbolded letter at the start of their title
-    prevSibling = titleTag.previous_sibling
-    if prevSibling != None:
-        courseTitle = prevSibling.string + courseTitle
-    
-    numTitleExtraEndBoldLetters = 0
-    
+    prevSibling: bs4.PageElement | None = titleTag.previous_sibling
+    if type(prevSibling) == bs4.NavigableString:
+        courseTitle = prevSibling + courseTitle
+
+    numTitleExtraEndBoldLetters: int = 0
+
     # Some courses have a bolded comma at the end of the title
     # Some also have an extra space as well
     if courseTitle[-1:] == ",":
@@ -58,54 +63,60 @@ def getCourseInfo(titleTag: bs4.Tag) -> dict:
     elif courseTitle[-2:] == ", ":
         courseTitle = courseTitle[:-2]
         numTitleExtraEndBoldLetters = 2
-    
-    courseInfo = list(titleTag.next_siblings)
 
-    courseCredit = ""
-    courseDesc = ""
-    
+    courseInfoTags: Final[list[bs4.PageElement]] = list(titleTag.next_siblings)
+
+    courseCredit: str = ""
+    courseDesc: str = ""
+
     # If a course credit is given, it must be the next sibling with index 0, and it must have the str type
     # Otherwise, we know it is a math credit
-    if type(courseInfo[0]) == bs4.NavigableString and not brInsideStrong:
-        courseCredit = courseInfo[0][(2 - numTitleExtraEndBoldLetters):]
-        
+    if type(courseInfoTags[0]) == bs4.NavigableString and not brInsideStrong:
+        courseCredit = courseInfoTags[0][(2 - numTitleExtraEndBoldLetters):]
+
         # Some course credits end with a space
         if courseCredit[-1:] == " ":
             courseCredit = courseCredit[:-1]
-        
+
         # Some end with " (", sometimes the case if it is open to 8th graders
         if courseCredit[-2:] == " (":
             courseCredit = courseCredit[:-2]
-    
     else:
         courseCredit = "Math"
-    
-    courseDescTags = []
+
+    courseDescElements: list[bs4.PageElement] = []
 
     if brInsideStrong:
-        courseDescTags = list(titleTag.next_siblings)
+        courseDescElements = list(titleTag.next_siblings)
     else:
-        courseDescTags = list(titleTag.findNextSibling(name="br").next_siblings)
-    
-    for thisCourseDescTag in courseDescTags:
-        if thisCourseDescTag.name != "br":
-            courseDesc += thisCourseDescTag.string
-    
-    # How do you even manage to put an invisible character at the end of the course description
+        brTag: bs4.Tag | bs4.NavigableString | None = titleTag.findNextSibling(name="br")
+        if brTag != None:
+            courseDescElements = list(brTag.next_siblings)
+        # Otherwise, the course should have no description, so courseDescTags is left empty, as is courseDesc
+
+    for thisCourseDescElement in courseDescElements:
+        if type(thisCourseDescElement) == bs4.NavigableString:
+            courseDesc += thisCourseDescElement
+        elif type(thisCourseDescElement) == bs4.Tag:
+            thisCourseDescElementStr: str | None = thisCourseDescElement.string
+            if thisCourseDescElementStr != None:
+                courseDesc += thisCourseDescElementStr
+
+    # How do you even manage to put an invisible character at the end of a course description
     if courseDesc[-1:] == " ":
         courseDesc = courseDesc[:-1]
-    
+
     return {COURSES_TITLE_KEY: courseTitle,
             COURSES_CREDIT_KEY: courseCredit,
             COURSES_DESC_KEY: courseDesc}
 
-titleTags = catalogSoup.find_all("strong")
+titleTags: Final[bs4.ResultSet[bs4.Tag]] = catalogSoup.find_all("strong")
 
 # One strong tag is an empty whitespace in the middle of the course catalog and should not be there
 for tagIndex, thisTag in enumerate(titleTags):
     if thisTag.string == " ":
         del(titleTags[tagIndex])
 
-courseDicts = list(map(getCourseInfo, titleTags))
-courseDF = pd.DataFrame(courseDicts)
+courseDicts: list[dict[str, str]] = list(map(getCourseInfo, titleTags))
+courseDF: pd.DataFrame = pd.DataFrame(courseDicts)
 courseDF.to_csv("courses.csv")
